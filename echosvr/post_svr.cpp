edd9d52f743f7df765c74b4424ca8f820ddebe88
lvyahui <lvyahui8@126.com>
2017-03-19 16:24:22 +0800

    update

---
+++ echosvr/post_svr.cpp
@@ -0,0 +1,42 @@
+#include <handy/handy.h>
+#include <sstream>
+
+using namespace std;
+using namespace handy;
+
+int main(int argc ,const char * argv []){
+	
+	int nThreads = 1000;
+	
+	if(argc > 1){
+		nThreads = atoi(argv[1]);
+	}
+		
+	setloglevel("TRACE");
+	MultiBase base(100);
+	HttpServer echoServer(& base);
+	int r = echoServer.bind("",8088);
+	exitif(r,"bind failed %d %s",errno,strerror(errno));
+	echoServer.onPost("/post_example", [](const HttpConnPtr& con) {
+        	string v = con.getRequest().version;
+        	HttpResponse resp;
+		stringstream outJson;
+		// string outJson ;
+		time_t t = time(0);
+		outJson << "{\"c\":0,\"t\":" << t << "}" ;
+       	 	//resp.body = Slice("{\"c\":0,\"t\":-28800}");
+       	 	// outJson += "{\"c\":0,\"t\":";
+		//outJson += itoa(t);
+		// outJson += "}";
+       	 	// resp.body = Slice(outJson.c_str());
+       	 	resp.body = Slice(outJson.str());
+        	con.sendResponse(resp);
+        	if (v == "HTTP/1.0") {
+            		con->close();
+        	}
+    	});
+    	Signal::signal(SIGINT, [&]{base.exit();});
+    	base.loop();
+
+	return 0;
+}
