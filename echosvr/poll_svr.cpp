85956893b0037962c8eb41255783c9606dfe6140
lvyahui <lvyahui8@126.com>
2017-04-14 13:34:29 +0800

    update

--- echosvr/poll_svr.cpp
+++ echosvr/poll_svr.cpp
@@ -11,11 +11,15 @@ int main(int argc ,const char * argv []){
 	if(argc > 1){
 		nThreads = atoi(argv[1]);
 	}
+	int port = 8081;
+	if(argc > 2){
+		port=atoi(argv[2]);	
+	}
 		
 	setloglevel("TRACE");
 	MultiBase base(100);
 	HttpServer echoServer(& base);
-	int r = echoServer.bind("",8088);
+	int r = echoServer.bind("",port);
 	exitif(r,"bind failed %d %s",errno,strerror(errno));
 	echoServer.onGet("/h5/reply/unread-with-time", [](const HttpConnPtr& con) {
         	string v = con.getRequest().version;
