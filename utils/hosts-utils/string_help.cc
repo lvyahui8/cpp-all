5b18216ccf364ed171c855a864ee3fb3a5b59b4a
lvyahui8 <lvyahui8@126.com>
2017-08-09 23:44:02 +0800

    update

--- utils/hosts-utils/string_help.cc
+++ utils/hosts-utils/string_help.cc
@@ -7,11 +7,26 @@ string trim(const string & str,const string chs = "\r\n\t\0"){
 	if ( str.empty() ) {
 		return str;
 	}
-  	for (string::iterator it=str.begin() ; it < str.end(); it++ ){
-		string::size_type pos = str.find_first_not_of( * it);	
-		if ( pos == string::n_pos ) {
-				
+	string tmp=str;
+  	for ( string::const_iterator it = chs.begin() ; 
+			it < chs.end(); it++ ){
+		string::size_type first_pos = tmp.find_first_not_of( * it);	
+		if ( first_pos == string::npos ) {
+				return "";
 		}
+		
+		string::size_type last_post = tmp.find_last_not_of( * it);
+
+		if (last_post != string::npos ){
+			tmp = tmp.substr(first_pos,last_post - first_pos + 1);
+		}
+
+		tmp = tmp.substr(first_pos);
 	}
-	
+	return str;
+}
+
+
+int split(const string & str,vector<string> ret,string dim = " " ){
+	return 0;
 }
--- utils/hosts-utils/string_help.h
+++ utils/hosts-utils/string_help.h
@@ -2,9 +2,12 @@
 #define __STRING_HELP_H_
 
 #include <string>
+#include <vector>
 using namespace std;
 
-string trim(const string & str,const string chs = "\r\n\t\0");
-int split(const string & str,vector<string> ret,string dim = " ");
+
+string trim(const string & str,const string chs);
+
+int split(const string & str,vector<string> ret,string dim);
 
 #endif // __STRING_HELP_H_
