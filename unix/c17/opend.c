8bf831c7acd1f1da149434d0a550ed494642cd11
lvyahui <lvyahui8@126.com>
2017-09-03 09:43:35 +0800

    update

--- unix/c17/opend.c
+++ unix/c17/opend.c
@@ -53,3 +53,14 @@ void handle_request(char * buf,int nread,int fd)
 	}
 	close(newfd);
 }
+
+int cli_args(int argc,char ** argv){
+	if (argc != 3 || strcmp(argv[0],CL_OPEN) != 0) {
+		strcpy(errmsg,"usage: <pathname> <oflag>\n");
+		return -1;
+	}
+	
+	pathname = argv[1];
+	oflag = atoi(argv[2]);
+	return 0;
+}
