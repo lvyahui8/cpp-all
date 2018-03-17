b2c292d81471a7c3130af3a425673f3e366a38f3
lvyahui <lvyahui8@126.com>
2018-03-18 00:38:26 +0800

    update

--- net/echo_svr/std_api/tcp_cli.c
+++ net/echo_svr/std_api/tcp_cli.c
@@ -25,11 +25,14 @@ int main(){
 	char * svrIp = "127.0.0.1";
 
 	svrfd = socket(AF_INET,SOCK_STREAM,0);	
+
 	bzero(&svraddr,sizeof(svraddr));
 	svraddr.sin_family = AF_INET;
 	svraddr.sin_port = htons(9876);
 	inet_aton(svrIp,& svraddr.sin_addr);
 
+	// connect 函数会填充套接字对应的tcp四元组，也就是选择一个可用的本地IP地址与一个随机端口（50000-65000）
+	// 当然，你可以在调用connect之前先调用bind，指明本地ip和端口，但这种操作并不常见
 	if(connect(svrfd,(struct sockaddr *) & svraddr,sizeof(struct sockaddr)) < 0 ){
 		printf("connect failed.");
 		exit(1);
